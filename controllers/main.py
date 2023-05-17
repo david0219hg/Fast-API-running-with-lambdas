e3602e1029244d87ceae4c5805899ff8d99aaa7b
David Hurtado <david0219hg@gmail.com>
2023-05-16 22:05:31 -0500

    fastapi project running and working

--- controllers/main.py
+++ controllers/main.py
@@ -3,8 +3,17 @@ from mangum import Mangum
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
+@app.get("/hello")
+def hello_world():
     return {"Hello": "World"}
+
+@app.get("/example")
+def example():
+    return {"example": "this is an example"}
+
+
+@app.get("/example/we-did-it")
+def example():
+    return {"winner": "it works"}
 
 handler = Mangum(app)
